683173c1022923dc4d17e14324a92e775b874edc
LeoGiliberti <128859071+LeoGiliberti@users.noreply.github.com>
2025-12-14 19:06:20 +0100

    Delete api/env.js

--- api/env.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const mask = v => Boolean(v && String(v).length > 5);
-  res.status(200).json({
-    CHATBASE_API_URL: mask(process.env.CHATBASE_API_URL),
-    CHATBASE_API_KEY: mask(process.env.CHATBASE_API_KEY),
-    CHATBASE_BOT_ID:  mask(process.env.CHATBASE_BOT_ID),
-    HEYGEN_API_KEY:   mask(process.env.HEYGEN_API_KEY)
-  });
-}
